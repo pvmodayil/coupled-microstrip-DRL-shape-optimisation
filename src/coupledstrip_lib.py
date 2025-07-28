061bd9feb18e3f58c21364c5a463ba6e76fd9d55
Philip Modayil <philipv.modayil@gmail.com>
2025-07-28 12:34:43 +0200

    Convexity check function

--- src/coupledstrip_lib.py
+++ src/coupledstrip_lib.py
@@ -20,6 +20,17 @@ def is_monotone(g: NDArray[np.float64], decreasing: bool) -> bool:
         return bool(np.all(dx < 0))
     return bool(np.all(dx > 0)) 
     
+def is_convex(g: NDArray[np.float64]) -> bool:
+    # Check if the array has at least 3 elements
+    if len(g) < 3:
+        raise ValueError("The array should have at least 3 elements to perform convexity check.\nPlease use more g-points")
+    
+    # Calculate the second differences
+    dx2 = g[2:] - 2 * g[1:-1] + g[:-2]
+    
+    # Check if all second differences are non-negative
+    # return the boolean value
+    return bool(np.all(dx2 >= 0))
 
 ######################################################################################
 #                              Potential & Potential Coeffs
