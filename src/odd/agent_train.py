914c24166ac8b4877499d36f990f85a881f06bfa
Philip Modayil <philipv.modayil@gmail.com>
2025-08-09 22:58:47 +0200

    Chnaged num points to 50

--- src/odd/agent_train.py
+++ src/odd/agent_train.py
@@ -310,7 +310,7 @@ if __name__ == "__main__":
         er1=1.0, # dielectric constatnt for medium 1
         er2=4.5, # dielctric constant for medium 2
         num_fs=2000, # number of fourier series coefficients
-        num_pts=100, # number of points for the piece wise linear approaximation
+        num_pts=50, # number of points for the piece wise linear approaximation
         mode="Odd"
     )
     main(CSA=CSA)
