159fc8a17365e039924525f4232d9e20a3b348df
Philip Modayil <philipv.modayil@gmail.com>
2025-08-11 11:29:43 +0200

    changed order of fucntions

--- src/odd/ga_lib/main.cpp
+++ src/odd/ga_lib/main.cpp
@@ -12,53 +12,6 @@
 // #include <pybind11/pybind11.h>
 // #include <pybind11/eigen.h>
 
-int main(){
-    std::cout << "Genetic Algorithm Optimization" << std::endl;
-    std::cout << "------------------------------" << std::endl;
-    
-    std::cout << "Using OpenMP with " << omp_get_max_threads() << " threads." << std::endl;
-    // Set the number of threads for OpenMP
-    omp_set_num_threads(omp_get_max_threads());
-
-    // Read starting curve cased D
-    std::string filename = "../data/CaseD_predicted_curve.csv";
-    std::cout<< "Reading g point values from: " << filename << std::endl;
-    std::unordered_map<std::string, std::vector<double>> dataD = fileio::read_csv(filename);
-
-    // Coupled Strip arrangement TC#1 Case D
-    double V0 = 1.0;
-    double space_bw_strps = 200e-6;
-    double width_micrstr = 150e-6;
-    double ht_micrstr = 0;
-    double hw_arra = 3e-3;
-    double ht_arra = 2.76e-3;
-    double ht_subs = 112e-6;
-    double er1 = 1.0;
-    double er2 = 4.5;
-    int num_fs = 2000; 
-    
-    // Convert the x and g vectors to Eigen arrays
-    Eigen::ArrayXd x_leftD = Eigen::Map<const Eigen::ArrayXd>(dataD["x_left"].data(), dataD["x_left"].size(), 1); // Mx1
-    Eigen::ArrayXd g_leftD = Eigen::Map<const Eigen::ArrayXd>(dataD["g_left"].data(), dataD["g_left"].size(), 1); // Mx1
-    Eigen::ArrayXd x_rightD = Eigen::Map<const Eigen::ArrayXd>(dataD["x_right"].data(), dataD["x_right"].size(), 1); // Mx1
-    Eigen::ArrayXd g_rightD = Eigen::Map<const Eigen::ArrayXd>(dataD["g_right"].data(), dataD["g_right"].size(), 1); // Mx1
-
-    int population_size = 100;
-    int num_generations = 1000;
-    GA::GAResult resultD = ga_optimize(V0,space_bw_strps,width_micrstr,ht_micrstr,hw_arra,ht_arra,ht_subs,er1,er2,num_fs,population_size,num_generations,
-    x_leftD,g_leftD,x_rightD,g_rightD);
-
-    std::vector<double> result_vec_leftD(resultD.best_curve_left.data(), resultD.best_curve_left.data() + resultD.best_curve_left.size());
-    std::vector<double> result_vec_rightD(resultD.best_curve_right.data(), resultD.best_curve_right.data() + resultD.best_curve_right.size());
-    std::unordered_map<std::string, std::vector<double>> result_dataD;
-    result_dataD["x_left"] = dataD["x_left"];
-    result_dataD["g_left"] = result_vec_leftD;
-    result_dataD["x_right"] = dataD["x_right"];
-    result_dataD["g_right"] = result_vec_rightD;
-    std::string curve_output_filename = "..data/CaseD_optimized_curve.csv";
-    fileio::write_csv(curve_output_filename, result_dataD);
-}
-
 GA::GAResult ga_optimize(const double V0,
             const double space_bw_strps,
             const double width_micrstr,
@@ -106,4 +59,51 @@ GA::GAResult ga_optimize(const double V0,
             std::cout << "Best energy: " << result.best_energy << "\n";
 
             return result;
+}
+
+int main(){
+    std::cout << "Genetic Algorithm Optimization" << std::endl;
+    std::cout << "------------------------------" << std::endl;
+    
+    std::cout << "Using OpenMP with " << omp_get_max_threads() << " threads." << std::endl;
+    // Set the number of threads for OpenMP
+    omp_set_num_threads(omp_get_max_threads());
+
+    // Read starting curve cased D
+    std::string filename = "../data/CaseD_predicted_curve.csv";
+    std::cout<< "Reading g point values from: " << filename << std::endl;
+    std::unordered_map<std::string, std::vector<double>> dataD = fileio::read_csv(filename);
+
+    // Coupled Strip arrangement TC#1 Case D
+    double V0 = 1.0;
+    double space_bw_strps = 200e-6;
+    double width_micrstr = 150e-6;
+    double ht_micrstr = 0;
+    double hw_arra = 3e-3;
+    double ht_arra = 2.76e-3;
+    double ht_subs = 112e-6;
+    double er1 = 1.0;
+    double er2 = 4.5;
+    int num_fs = 2000; 
+    
+    // Convert the x and g vectors to Eigen arrays
+    Eigen::ArrayXd x_leftD = Eigen::Map<const Eigen::ArrayXd>(dataD["x_left"].data(), dataD["x_left"].size(), 1); // Mx1
+    Eigen::ArrayXd g_leftD = Eigen::Map<const Eigen::ArrayXd>(dataD["g_left"].data(), dataD["g_left"].size(), 1); // Mx1
+    Eigen::ArrayXd x_rightD = Eigen::Map<const Eigen::ArrayXd>(dataD["x_right"].data(), dataD["x_right"].size(), 1); // Mx1
+    Eigen::ArrayXd g_rightD = Eigen::Map<const Eigen::ArrayXd>(dataD["g_right"].data(), dataD["g_right"].size(), 1); // Mx1
+
+    int population_size = 100;
+    int num_generations = 1000;
+    GA::GAResult resultD = ga_optimize(V0,space_bw_strps,width_micrstr,ht_micrstr,hw_arra,ht_arra,ht_subs,er1,er2,num_fs,population_size,num_generations,
+    x_leftD,g_leftD,x_rightD,g_rightD);
+
+    std::vector<double> result_vec_leftD(resultD.best_curve_left.data(), resultD.best_curve_left.data() + resultD.best_curve_left.size());
+    std::vector<double> result_vec_rightD(resultD.best_curve_right.data(), resultD.best_curve_right.data() + resultD.best_curve_right.size());
+    std::unordered_map<std::string, std::vector<double>> result_dataD;
+    result_dataD["x_left"] = dataD["x_left"];
+    result_dataD["g_left"] = result_vec_leftD;
+    result_dataD["x_right"] = dataD["x_right"];
+    result_dataD["g_right"] = result_vec_rightD;
+    std::string curve_output_filename = "..data/CaseD_optimized_curve.csv";
+    fileio::write_csv(curve_output_filename, result_dataD);
 }
