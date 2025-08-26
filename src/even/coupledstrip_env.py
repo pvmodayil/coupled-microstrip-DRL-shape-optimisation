a1c2864df9a706f10c584a1fce62ec530eccba51
Philip Modayil <philipv.modayil@gmail.com>
2025-08-26 14:20:36 +0200

    print g0

--- src/even/coupledstrip_env.py
+++ src/even/coupledstrip_env.py
@@ -310,7 +310,7 @@ class CoupledStripEnv(Env):
         CONSTRAINT_SCALING_FACTOR: float = 2 
         reward: float
         penality: float
-        reward_boost: float = 0
+        reward_boost: float = 1
         
         # To promote some change
         if np.all(action == 0):
@@ -324,15 +324,15 @@ class CoupledStripEnv(Env):
                                                     g_right=g_right,
                                                     x_right=x_right)
             if energy < self.minimum_energy[-1]:
-                logger.info(f"New minimum energy obtained: {energy} VAs\n")
+                logger.info(f"New minimum energy obtained: {energy} VAs with G0: {action[0]} \n")
                 self.minimum_energy = np.append(self.minimum_energy, energy)
-                reward_boost = 0.5
+                reward_boost = 2
                 
             # Max val = -0.5 + 2/4 = 0 , if monotonicity satisfied base value will be -0.5
             constraint: float = self._soft_plus(MAX_CONVEXITY_PENALITY + (csa_lib.degree_convexity(g=g_left)/self.CSA.num_pts 
                         + csa_lib.degree_convexity(g=g_left)/self.CSA.num_pts)*SCALING_FACTOR)*CONSTRAINT_SCALING_FACTOR   
             # Squashing to the bounds of [0,1]
-            reward = self._soft_plus((self.energy_baseline/energy) + reward_boost + constraint) # (1/energy)/(1/self.energy_baseline) energy decrease value increase
+            reward = self._soft_plus((self.energy_baseline/energy)*reward_boost + constraint) # (1/energy)/(1/self.energy_baseline) energy decrease value increase
         else:
             # Max val = -1 + 2/4 = -0.5
             penality = MAX_PENALITY + (csa_lib.degree_monotonicity(g=g_left,type='increasing')/self.CSA.num_pts 
