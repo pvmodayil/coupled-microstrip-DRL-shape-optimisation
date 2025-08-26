a68675388f06286998b55f4fb3c951bad755669a
Philip Modayil <philipv.modayil@gmail.com>
2025-08-26 15:28:37 +0200

    auto

--- src/even/_hyper_parameter.py
+++ src/even/_hyper_parameter.py
@@ -1,6 +1,6 @@
 policy_kwargs: dict[str, int | list[int]] = dict(log_std_init=-3, net_arch=[400, 300]) # setting nn architecture
 hyperparams: dict[str, int|float|bool|str] = {
-    'ent_coef': 0.2,  # setting entropy coefficient to auto boosts exploration
+    'ent_coef': 'auto_0.01',  # setting entropy coefficient to auto boosts exploration
     'target_entropy':-9,
     'batch_size':512,  
     'buffer_size': 20000,        
