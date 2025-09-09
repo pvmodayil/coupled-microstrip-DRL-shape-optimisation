8434f29cb86d055e1af642bb939302740ed72f58
Philip Modayil <philipv.modayil@gmail.com>
2025-09-09 12:40:05 +0200

    base ntwork architecture with constraint scaled rewards

--- src/even/coupledstrip_env.py
+++ src/even/coupledstrip_env.py
@@ -43,7 +43,7 @@ class CoupledStripEnv(Env):
         
         # Calculate the baseline energy for scaling reward
         action_left: np.ndarray = np.array([0.4, 0.1, 0.1, 0.1, 0.1]) # P0Y, P1X, deviation of P1Y from P0Y, deviation of P2X from P1X, deviation of P2Y from P1Y
-        action_right: NDArray = np.zeros(4)
+        action_right: NDArray = np.zeros(4, dtype=np.float64)
         x_left: NDArray
         g_left: NDArray
         _control: NDArray
@@ -70,8 +70,7 @@ class CoupledStripEnv(Env):
                                                     ht_subs=self.CSA.ht_subs,
                                                     vn=vn)
         # logger.info(f"Initial energy: {self.energy_baseline} VAs")
-        self.minimum_energy: NDArray = np.array([np.inf])
-        self.energy_calculation_count: int = 0
+        self.minimum_energy: NDArray = np.array([self.energy_baseline])
         
         # Define action and observation space
         """
@@ -308,6 +307,7 @@ class CoupledStripEnv(Env):
         MAX_CONVEXITY_PENALITY: float = -0.5
         # each check will have max value 1 so total max will be 2, need it to be constarined to 0.5 so that each check contributes +0.5 from MAX_PENALITY
         SCALING_FACTOR: float = 0.25
+        CONSTRAINT_SCALING_FACTOR: float = 2 
         reward: float
         penality: float
         reward_boost: float = 1
@@ -323,20 +323,17 @@ class CoupledStripEnv(Env):
                                                     x_left=x_left,
                                                     g_right=g_right,
                                                     x_right=x_right)
-            if csa_lib.is_convex(g=g_left) and csa_lib.is_convex(g=g_right):
-                if (energy < self.minimum_energy[-1]) and (self.energy_calculation_count == 1):
-                    logger.info(f"New minimum energy obtained: {energy} VAs with G0: {action[0]} \n")
-                    self.minimum_energy = np.append(self.minimum_energy, energy)
-                    reward_boost = 2
+            if energy < self.minimum_energy[-1]:
+                logger.info(f"New minimum energy obtained: {energy} VAs\n")
+                self.minimum_energy = np.append(self.minimum_energy, energy)
+                reward_boost = 4
                 
-                if self.energy_calculation_count == 0:
-                    self.energy_calculation_count = 1
-                    
-                reward = self._soft_plus(self.energy_baseline/energy)*reward_boost
-            else:
-                penality = MAX_CONVEXITY_PENALITY + (csa_lib.degree_convexity(g=g_left)/self.CSA.num_pts 
-                            + csa_lib.degree_convexity(g=g_left)/self.CSA.num_pts)*SCALING_FACTOR
-                reward = penality
+            # Max val = -0.5 + 2/4 = 0 , if monotonicity satisfied base value will be -0.5
+            constraint: float = self._soft_plus(MAX_CONVEXITY_PENALITY + (csa_lib.degree_convexity(g=g_left)/self.CSA.num_pts 
+                        + csa_lib.degree_convexity(g=g_left)/self.CSA.num_pts)*SCALING_FACTOR)*CONSTRAINT_SCALING_FACTOR   
+            
+            # Smooth gradient rewards with soft plus function
+            reward = self._soft_plus((self.energy_baseline/energy)*reward_boost + constraint) 
         else:
             # Max val = -1 + 2/4 = -0.5
             penality = MAX_PENALITY + (csa_lib.degree_monotonicity(g=g_left,type='increasing')/self.CSA.num_pts 
