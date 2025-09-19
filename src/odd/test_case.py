42c82182718d34023a422affab37c3e913502475
Philip Modayil <philipv.modayil@gmail.com>
2025-09-19 13:18:46 +0200

    plot label and tick font size set

--- src/odd/test_case.py
+++ src/odd/test_case.py
@@ -193,7 +193,7 @@ def hybrid_algorithm(env: CoupledStripEnv, model: SAC, image_dir: str, case: str
     num_fs: int = 1500
     noise_scale: float = 0.9
     population_size: int = 100
-    num_generations: int = 600
+    num_generations: int = 300
     start_time_GA: float = time.perf_counter()
     result: GAOptResult = ga_cpp.ga_optimize(env.CSA.V0,
                                     env.CSA.space_bw_strps,
